a60342394179fe4651109bea7859c6054a478b78
wjncet <wjncet@users.noreply.github.com>
2021-01-08 11:06:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     databaseURL: process.env.DATABASE_URL
         || "postgres://postgres:postgres@127.0.0.1:5432/postgres"
     ,
-    bPool: {
+    dbPool: {
       max: parseInt(process.env.DBPOOL_MAX) || 50,
       connectionTimeoutMillis: parseInt(process.env.DBPOOL_CONNECTION_TIMEOUT_MILLIS) || 0,
       idleTimeoutMillis: parseInt(process.env.DBPOOL_IDLE_TIMEOUT_MILLIS) || 10000,
